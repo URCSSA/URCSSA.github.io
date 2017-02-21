62cf7edd84c756f92cea205305d52ed947f57ca1
NicholasJW <yunwen.wan@richmond.edu>
2017-02-21 17:24:03 -0500

    JS 1st draft

---
+++ mainContent.js
@@ -0,0 +1,30 @@
+/*
+var eventBox = document.getElementById("eventBox");
+var eventTextContainer = document.getElementById("eventTextContainer");
+var eventText = document.getElementById("eventText");
+*/
+
+//Navigation Bar variables.
+var sideBarContainer = document.getElementById("sideBarContainer");
+var navBar = document.getElementById("navBar");
+
+var homeDrop = navBar.getChildren()[0].getChildren()[0];
+var homeButton = homeDrop.getChildren()[0];
+
+var aboutDrop = navBar.getChildren()[0].getChildren()[1];
+var aboutButton = aboutDrop.getChildren()[0];
+var missonAndVisionButton = aboutDrop.getChildren()[1].getChildren()[0];
+var meetOurTeamButton = aboutDrop.getChildren()[1].getChildren()[1];
+
+var informationDrop = navBar.getChildren()[0].getChildren()[2];
+var informationButton = informationDrop.getChildren()[0];
+var resourcesButton = informationDrop.getChildren()[1].getChildren()[0];
+var faqButton = informationDrop.getChildren()[1].getChildren()[1];
+
+var activityDrop = navBar.getChildren()[0].getChildren()[3];
+var activityButton = activityDrop.getChildren()[0];
+var pastActivityButton = activityDrop.getChildren()[1].getChildren()[0];
+
+var ContactDrop = navBar.getChildren()[0].getChildren()[4];
+var ContactButton = ContactDrop.getChildren()[0];
+
